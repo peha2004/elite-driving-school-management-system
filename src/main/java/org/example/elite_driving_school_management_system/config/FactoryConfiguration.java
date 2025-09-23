9050465b713812adadc2fe7cb77a7f9a2652381f
Ranuthi Pehansa <ranuthi2004@gmail.com>
2025-09-23 18:51:13 +0530

    Updated FactoryConfiguration.java for Hibernate session management

--- src/main/java/org/example/elite_driving_school_management_system/config/FactoryConfiguration.java
+++ src/main/java/org/example/elite_driving_school_management_system/config/FactoryConfiguration.java
@@ -1,4 +1,57 @@
 package org.example.elite_driving_school_management_system.config;
 
+import org.example.elite_driving_school_management_system.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Properties;
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+
+    private FactoryConfiguration()  {
+        Properties prop = new Properties();
+
+        try {
+            prop.load(
+                    FactoryConfiguration.class.getClassLoader().getResourceAsStream("hibernate.properties")
+            );
+
+            Configuration configuration = new Configuration();
+            configuration.addProperties(prop);
+
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Student.class);
+            configuration.addAnnotatedClass(Course.class);
+            configuration.addAnnotatedClass(Instructor.class);
+            configuration.addAnnotatedClass(Lesson.class);
+            configuration.addAnnotatedClass(Payment.class);
+
+
+            sessionFactory = configuration.buildSessionFactory();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Error in hibernate properties",e);
+        }
+    }
+
+    public static FactoryConfiguration getInstance()  {
+        return factoryConfiguration == null ?
+                factoryConfiguration = new FactoryConfiguration()
+                :
+                factoryConfiguration;
+    }
+
+    public Session getSession(){
+        Session session = sessionFactory.openSession();
+        return session;
+    }
+
+    public Session getCurrentSession(){
+        return sessionFactory.getCurrentSession();
+    }
 }
