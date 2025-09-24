464452605da888eeee3ecead6351e51a1c49526a
Ranuthi Pehansa <ranuthi2004@gmail.com>
2025-09-24 23:04:11 +0530

    To get Studnet Count Done

--- src/main/java/org/example/elite_driving_school_management_system/dao/custom/StudentDAO.java
+++ src/main/java/org/example/elite_driving_school_management_system/dao/custom/StudentDAO.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface StudentDAO extends CrudDAO<Student,String> {
     String generateNewId() throws Exception;
     List<String> getEnrolledCourseIds(String studentId) throws Exception;
+    int count() throws Exception;
 }
