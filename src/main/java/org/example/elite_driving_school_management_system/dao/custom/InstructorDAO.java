17ba4004f383ecb5b73701dc9ada6a0680f134f3
Ranuthi Pehansa <ranuthi2004@gmail.com>
2025-09-24 22:09:55 +0530

    Updated InstructorDAO

--- src/main/java/org/example/elite_driving_school_management_system/dao/custom/InstructorDAO.java
+++ src/main/java/org/example/elite_driving_school_management_system/dao/custom/InstructorDAO.java
@@ -4,4 +4,5 @@ import org.example.elite_driving_school_management_system.dao.CrudDAO;
 import org.example.elite_driving_school_management_system.entity.Instructor;
 
 public interface InstructorDAO extends CrudDAO<Instructor,String> {
+    int count() throws Exception;
 }
