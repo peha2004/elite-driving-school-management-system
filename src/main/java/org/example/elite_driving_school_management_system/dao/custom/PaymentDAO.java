3050ebaa8dbaeece3abcccaf7b01b6b7adcba9d4
Ranuthi Pehansa <ranuthi2004@gmail.com>
2025-09-24 23:02:47 +0530

    Updated getIncome By Day

--- src/main/java/org/example/elite_driving_school_management_system/dao/custom/PaymentDAO.java
+++ src/main/java/org/example/elite_driving_school_management_system/dao/custom/PaymentDAO.java
@@ -4,8 +4,11 @@ import org.example.elite_driving_school_management_system.dao.CrudDAO;
 import org.example.elite_driving_school_management_system.entity.Payment;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PaymentDAO  extends CrudDAO<Payment,String> {
     String generateId() throws Exception;
     List<Payment> getPaymentsByStudent(String studentId) throws Exception;
+    double getTotalPayments() throws Exception;
+    Map<String, Double> getIncomeByDay() throws Exception;
 }
